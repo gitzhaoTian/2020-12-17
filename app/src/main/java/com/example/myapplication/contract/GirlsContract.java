23f3275d80c9c132baa6a925cea878408746561c
gitzhaoTian <2171262990@qq.com>
2020-12-17 19:37:03 +0800

    19/36

--- app/src/main/java/com/example/myapplication/contract/GirlsContract.java
+++ app/src/main/java/com/example/myapplication/contract/GirlsContract.java
@@ -1,7 +1,17 @@
 package com.example.myapplication.contract;
 
+import com.example.myapplication.bean.GirlsBean;
+import com.example.myapplication.utils.INetCallBack;
+
 public class GirlsContract {
     public interface IGirlsModel{
-        <T> void getGirls(String url,);
+        <T> void getGirls(String url, INetCallBack<T> callBack);
+    }
+    public interface IGirlsPresenter{
+        void getGirls();
+    }
+    public interface IGirlsView{
+        void getGirls(GirlsBean girlsBean);
+        void onFail(String error);
     }
 }
